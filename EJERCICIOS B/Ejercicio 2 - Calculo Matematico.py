7fe45f0aef7f2c1fa0b027fac52406bf66800ffd
archivopsd <79292091+archivopsd@users.noreply.github.com>
2021-09-07 10:06:39 -0500

    Update
    Update

---
+++ EJERCICIOS B/Ejercicio 2 - Calculo Matematico.py
@@ -0,0 +1,7 @@
+x = 5
+y = 5
+
+def primera_funcion():
+    print((x + y) / (x ** 2 - y))
+    
+primera_funcion()
---
+++ EJERCICIOS B/Ejercicio 3 - Cuadrado de un Número.py
@@ -0,0 +1,6 @@
+x = 5
+
+def primera_funcion():
+    print(x**2)
+    
+primera_funcion()
