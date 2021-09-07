f121d47644081ced2fc79dfb341350d05d54afef
archivopsd <79292091+archivopsd@users.noreply.github.com>
2021-09-06 22:20:06 -0500

    Actualizacion de ejercicios
    Actualizacion de ejercicios

--- Ejercicio 1 - Calcular IMC.py
+++ Ejercicio 1 - Calcular IMC.py
@@ -1,4 +1,14 @@
 altura = float(input("favor de escribir su estatura en metros:"))
 peso = float(input("favor de escribir su peso en kilogramos:"))
-imc = peso /  altura ** 2  
-print(imc)
+
+x = peso /  altura ** 2  
+print(x)
+
+if (x >= 0 ) and (x<= 18.9 ):
+    print('Su IMC esta por debajo de lo normal.')
+if (x >= 19) and (x<=24.9 ):
+    print('Su IMC esta en nivel normal.')
+if (x >= 25) and (x<=29.9 ):
+    print('Su IMC esta por encima de lo normal.')
+if (x>=30 ):
+    print('Su IMC indica obesidad, favor de ir a con un médico.')
